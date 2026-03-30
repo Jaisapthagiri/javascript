a8f11e60a7e14556ae78a10d29a03e9541cace0a
jaisapthagiri <imjai6504@gmail.com>
2026-03-30 11:13:01 +0530

    objs workd

---
+++ objects/constructor.js
@@ -0,0 +1,22 @@
+// let user = {
+//     name : "jai"
+// }
+
+// user = null;
+// console.log(user);
+
+// let a = {};
+// let b = {};
+// a.ref = b;
+
+// class User {
+//     constructor(name) {
+//         this.name = name;
+//         console.log(this.name);
+//     }   
+// }
+
+// const a = new User("Jai")
+// const b = new User("Some")
+
+// console.log(typeof null);
--- objects/objs.js
+++ objects/objs.js
@@ -1,2 +1,45 @@
-let a;
-console.log(a);
+let a = 10;
+let b = a;
+b = 20;
+
+// console.log(a);
+// console.log(b);
+
+const objs = {
+    a: "name",
+    b: "age",
+    c: "city",
+}
+
+const objs1 = objs;
+
+objs1.c = "address";
+
+delete objs1.c;
+// console.log(objs);
+// console.log(objs1);
+
+// console.log("a" in objs1); // check prop exist
+// console.log(objs["a"]);    // backet notation
+
+objs.d = "new one"
+// console.log(objs);
+delete objs1.d;
+// console.log(objs1);
+
+const objs2 = {
+    "name": "tennyson",
+    "age": 15,
+    "city": "bellwood",
+    "language": "tamil",
+    "gender": "male",
+    greet() {
+        return "The Name is " + this.name + " age is " + this.age;
+    }
+}
+
+// console.log(objs2.greet());
+// console.log("A" , "B");
+// console.log(("A" , "B"));
+
+
