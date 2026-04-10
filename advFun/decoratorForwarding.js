b1b32e43633b0e3a166d22e2cc8e98e7aefd48b1
jaisapthagiri <imjai6504@gmail.com>
2026-04-10 17:57:23 +0530

    nothing

--- advFun/decoratorForwarding.js
+++ advFun/decoratorForwarding.js
@@ -1,68 +1,75 @@
-// decorator
+// // decorator
 
-function greet(name) {
-    return `Hello ${name}`;
-}
+// function greet(name) {
+//     return `Hello ${name}`;
+// }
 
-function decorator(greet) {
-    return function (name) {
-        console.log("calling");
-        return greet(name)
-    }
-}
+// function decorator(greet) {
+//     return function (name) {
+//         console.log("calling");
+//         return greet(name)
+//     }
+// }
 
-const callingFun = decorator(greet)
-// console.log(callingFun("Arthur"));
-
-
-// forwarding
-
-function calling(a, b) {
-    return a + b;
-}
-
-function wrapper(fn) {
-    return function (...args) {
-        return fn(...args)
-    }
-}
-
-const warpped = wrapper(calling)
-// console.log(warpped(2, 3));
+// const callingFun = decorator(greet)
+// // console.log(callingFun("Arthur"));
 
 
-// call
+// // forwarding
+
+// function calling(a, b) {
+//     return a + b;
+// }
+
+// function wrapper(fn) {
+//     return function (...args) {
+//         return fn(...args)
+//     }
+// }
+
+// const warpped = wrapper(calling)
+// // console.log(warpped(2, 3));
+
+
+// // call
 
 const objec = {
     uName: "Arthur",
     lName: "Morgan",
     work: "rdr2",
     greet(msg) {
-        return `${msg} Mr.${uName} ${lName} `
+        return `${msg} Mr.${this.uName} ${this.lName} `
     }
 }
 
-console.log(objec.greet.call(objec, "Hello"))
+const user = {
+    uName : "asdf",
+    lName : "asdfg"
+} 
+
+console.log(objec.greet.call(user, "Hello"))
 console.log(objec.greet("Hello"));
 
 
 // apply
 
-const stud = {
-    name: "Max",
-    tot(...vals) {
-        let sum = 0;
-        for (let i = 0; i < vals.length; i++) {
-            sum += vals[i]
-        }
-        return sum
-    },
-    get marks(){
-        return `${this.name} obtained ${this.tot(...arr)}`
-    }
-}
+// const stud = {
+//     name: "Max",
+//     // sum :10,
+//     tot(...vals) {
+//         let sum = 0;
+//         for (let i = 0; i < vals.length; i++) {
+//             sum += vals[i]
+//         }
+//         this.sum = sum;
+//         return sum
+//     },
+//     get marks(){
+//         return `${this.name} obtained ${this.sum}`
+//     }
+// }
 
-const arr = [80, 89, 75, 85, 86, 90]
+// const arr = [80, 89, 75, 85, 86, 90]
 
 // console.log(stud.tot.apply(stud, arr))
 // console.log(stud.marks);
